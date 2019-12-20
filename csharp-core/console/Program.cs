608a57042e24e297a668c319d1da84cd65d87ed3
Chris Hamons <chris.hamons@xamarin.com>
2019-12-20 12:09:16 -0600

    Add .net core project

---
+++ csharp-core/console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(library.Class1.AddTwo (4));
+        }
+    }
+}
---
+++ csharp-core/library/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace library
+{
+    public class Class1
+    {
+	    public static int AddTwo (int x) => x + 2;
+    }
+}
